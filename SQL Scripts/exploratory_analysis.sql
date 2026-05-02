8c7be9a86e68d0ce6958df558129f03867f3efb6
smahmoud31 <253846451+smahmoud31@users.noreply.github.com>
2026-05-02 18:01:31 -0400

    use correct table
    using cleaned table to preserve original data

--- SQL Scripts/exploratory_analysis.sql
+++ SQL Scripts/exploratory_analysis.sql
@@ -1,6 +1,4 @@
 --this script's purpose is to query the data to find trends/patterns to further investigate
 
-SELECT * FROM diabetes_data
-WHERE readmitted != 'NO';
---returned everything???
+SELECT * FROM clean_diabetes_data LIMIT 1;
 
